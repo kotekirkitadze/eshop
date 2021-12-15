c9fe7b8d44444c56c4f6cdcc1fb81a344c3f8aae
kotekirkitadze <kotekirkitadze@gmail.com>
2021-12-15 16:59:04 +0400

    libraries and some components generated

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { UiModule } from '@appbit/ui';
+
+const routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HomePageComponent, ProductListComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
