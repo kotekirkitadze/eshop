1c0fb2fd38d63ed18155b2d3ed40ca45b9675ab3
kotekirkitadze@gmail.com <kotekirkitadze@gmail.com>
2022-06-14 20:46:00 +0400

    profile pic change component updated

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eshop-backend-appbit.herokuapp.com/api/v1/',
+  apiUrl: 'http://localhost:3000/api/v1/',
+  // apiUrl: 'https://eshop-backend-appbit.herokuapp.com/api/v1/',
 };
